3370d989894615af3b5f0c1ed714f1a9e567cfc5
suranga prasad <surangaprasad55@gmail.com>
2024-01-06 21:36:11 +0200

    Update helper.py with plot graphs function
    Update helper.py with plot graphs function

--- modular/helper.py
+++ modular/helper.py
@@ -88,3 +88,51 @@ def create_dataloaders(
   )
 
   return train_dataloader, test_dataloader, valid_dataloader
+
+
+import matplotlib.pyplot as plt
+
+# Plot loss curves of a model
+def plot_loss_curves(results):
+    """Plots training curves of a results dictionary.
+
+    Args:
+        results (dict): dictionary containing list of values, e.g.
+            {"train_loss": [...],
+             "train_acc": [...],
+             "val_loss": [...],
+             "val_acc": [...]}
+    """
+
+
+    epochs = range(len(results["train_loss"]))
+
+    train_loss = []
+    test_loss = []
+    train_accuracy = []
+    test_accuracy = []
+
+
+    for i in epochs:
+      train_loss.append(results["train_loss"][i].detach().cpu().numpy())
+      test_loss.append(results["val_loss"][i].detach().cpu().numpy())
+      train_accuracy.append(results["train_acc"][i].detach().cpu().numpy())
+      test_accuracy.append(results["val_acc"][i].detach().cpu().numpy())
+
+    plt.figure(figsize=(15, 7))
+
+    # Plot loss
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, train_loss, label="train_loss")
+    plt.plot(epochs, test_loss, label="test_loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+    # Plot accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, train_accuracy, label="train_accuracy")
+    plt.plot(epochs, test_accuracy, label="test_accuracy")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
